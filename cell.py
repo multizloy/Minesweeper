ec4000b6fb1d0d26b9b70567c431b6756df56c18
Renat Shabanov <multizloy@gmail.com>
2024-07-10 17:59:28 +0300

    создали функции для показа количества мин вокруг нажатой клетки

--- cell.py
+++ cell.py
@@ -28,6 +28,44 @@ class Cell:
     def leftClickAction(self, Event):
         if self.isMine:
             self.showMine()
+        else:
+            self.showCell()
+
+    # описание окружающих клеток
+    @property
+    def surroundedCells(self):
+        cells = [
+            self.getCellByAxis(self.x - 1, self.y - 1),
+            self.getCellByAxis(self.x - 1, self.y),
+            self.getCellByAxis(self.x - 1, self.y + 1),
+            self.getCellByAxis(self.x, self.y - 1),
+            self.getCellByAxis(self.x + 1, self.y - 1),
+            self.getCellByAxis(self.x + 1, self.y),
+            self.getCellByAxis(self.x + 1, self.y + 1),
+            self.getCellByAxis(self.x, self.y + 1),
+        ]
+        cells = [cell for cell in cells if cell is not None]
+        return cells
+
+    # подсчет мин
+    @property
+    def surroundedCellsMinesLength(self):
+        counter = 0
+        for cell in self.surroundedCells:
+            if cell.isMine:
+                counter += 1
+        return counter
+
+    def showCell(self):
+        self.cellButtonObj.configure(
+            text=self.surroundedCellsMinesLength,
+        )
+
+    def getCellByAxis(self, x, y):
+        # вернуть клетку со значением х,у
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
 
     def showMine(self):
         # logic to interrupt the game
